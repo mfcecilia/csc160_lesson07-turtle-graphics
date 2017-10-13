b4b9b4509b4a358def94f65b4730f270e911793a
mcfernan <mykayla.c.fernandes@intel.com>
2017-10-12 19:56:07 -0600

    first commit

---
+++ turtle-graphics.java
@@ -0,0 +1,48 @@
+/*turtle-graphics.java
+@author: Evelyn Garcia
+@version: 2017.10.12
+*/
+
+
+/*turtle-graphics class*/
+class TurtleGraphics{
+
+  //floor contructor
+  //turtle contructor
+
+  /*command 1*/
+  void penUp(){
+  //pen up
+  }
+
+  /*command 2*/
+  void penDown(){
+  //pen down
+  }
+
+  /*command 3*/
+  void turnRight(){
+  //turnRight
+  }
+
+  /*command 4*/
+  void turnLeft(){
+  //turnLeft
+  }
+
+  /*command 5*/
+  void moveForward(){
+  //moveForward
+  }
+
+  /*command 6*/
+  void displayFloor(){
+  //displayFloor
+  }
+
+  /*command 9*/
+  void end(){
+  //end
+  }
+
+}
