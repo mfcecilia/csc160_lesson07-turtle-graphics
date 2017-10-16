449eb209e78eeadfab80ddfa5c246301380f2104
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2017-10-15 22:56:12 -0600

    displayFloor() complete, still figuring things out

--- turtle-graphics.java
+++ turtle-graphics.java
@@ -3,46 +3,90 @@
 @version: 2017.10.12
 */
 
+import java.util.*;
+
 
 /*turtle-graphics class*/
-class TurtleGraphics{
+class TurtleGraphics {
 
-  //floor contructor
-  //turtle contructor
+  //pen status
+  private static boolean draw = false;
 
-  /*command 1*/
-  void penUp(){
-  //pen up
+  ///turtle coordinates
+  private static int x, y;
+
+  //commands array
+  var commands[];
+
+  //floor 20x20 array
+  private static int floor[][] = new int [20][20];
+
+  /**
+     * Creates a new Turtle object on the floor.
+     * @param x     The x-coordinate of the turtle.
+     * @param y     The y-coordinate of the turtle.
+     */
+    public Turtle(int xx, int yy)
+    {
+        x = xx;
+        y = yy;
+    }
+
+
+  /*command 1 -- pen up*/
+  public void penUp(){
+  //don't draw
+  draw = false;
   }
 
   /*command 2*/
-  void penDown(){
-  //pen down
+  public void penDown(){
+  //draw
+  draw = true;
   }
 
   /*command 3*/
-  void turnRight(){
+  public void turnRight(){
   //turnRight
   }
 
   /*command 4*/
-  void turnLeft(){
+  public void turnLeft(){
   //turnLeft
   }
 
   /*command 5*/
-  void moveForward(){
+  public void moveForward(){
   //moveForward
   }
 
-  /*command 6*/
-  void displayFloor(){
+  /*command 6 -- display 20x20 floor array*/
+  public void displayFloor(){
   //displayFloor
+  for(int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            System.out.print(floor[i][j] + " ");
+            System.out.println();
+        }
+    }
   }
 
   /*command 9*/
-  void end(){
+  public void end(){
   //end
+  }
+
+  public void myProgram() {
+    Turtle evelyn = new Turtle(0,0);
+    this.add(evelyn);
+    evelyn.moveForward();
+  }
+
+  public static void main(String[] args) {
+
+    //scan commands
+    Scanner scan = new Scanner(System.in);
+
   }
 
 }
